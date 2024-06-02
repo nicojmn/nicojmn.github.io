25acb3785fe8fc2cb6884915b25644f9e8f499a7
nicojmn <nicolasjeanmenne@gmail.com>
2024-06-02 23:45:26 +0200

    margin and fix typo in es locale

--- src/lib/components/UnderConstruction.svelte
+++ src/lib/components/UnderConstruction.svelte
@@ -2,7 +2,7 @@
 	import { t } from "$lib/lang/translation";
 </script>
 
-<div role="alert" class="alert alert-warning">
+<div role="alert" class="alert alert-warning my-2">
 	<span class="text-4xl">🚧</span>
 	<span>{$t("under-construction")}</span>
 </div>
