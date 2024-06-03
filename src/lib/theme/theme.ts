f9dfe5148ab320c3756e9d863010bda6a3847d23
nicojmn <nicolasjeanmenne@gmail.com>
2024-06-03 22:10:44 +0200

    footer and it's icons

--- src/lib/theme/theme.ts
+++ src/lib/theme/theme.ts
@@ -1,7 +1,22 @@
-import lightmode from "./lightmode.svg";
-import darkmode from "./darkmode.svg";
+import lm from "./lightmode.svg";
+import dm from "./darkmode.svg";
+import gl from "./gitlab.svg";
+import gh from "./github.svg";
+import ghlight from "./github-light.svg";
+import linkedin from "./linkedin.svg";
+
 
 export const theme = {
-    light_icon: lightmode,
-    dark_icon: darkmode,
+    mode : {
+        light_icon: lm,
+        dark_icon: dm
+    },
+    icons: {
+        gitlab: gl,
+        github: {
+            light: ghlight,
+            dark: gh
+        },
+        linkedin: linkedin
+    }
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
     import { theme } from "$lib/theme/theme";
 
     let languageModal: HTMLDialogElement | null;
+    $:mode = "dark";
 
     function switchTranslation(lang: string) {
         loadTranslations(lang);
@@ -23,7 +24,7 @@
 <UnderConstruction />
 
 
-
+<!-- Header (tabs) -->
 <div role="tablist" class="tabs tabs-bordered">
     <a role="tab" href="/" class="tab tab-active">{$t("tabs.home")}</a>
     <a role="tab" href="#" class="tab">{$t("tabs.resume")}</a>
@@ -46,18 +47,47 @@
     </div>
     <div role="tab" class="tab">
         <label class="swap swap-rotate">
-            <input type="checkbox" value="light" class="theme-controller"/>
+            <input type="checkbox" value="light" class="theme-controller" on:click={() => mode === "light" ? mode = "light" : mode = "dark"}/>
 
-            <img src={theme.light_icon} alt="light theme icon" class="swap-off w-14 h-5/6 object-contain"/>
-            <img src={theme.dark_icon} alt="dark theme icon" class="swap-on w-14 h-5/6 object-contain"/>
+            <img src={theme.mode.light_icon} alt="light theme icon" class="swap-off w-14 h-5/6 object-contain"/>
+            <img src={theme.mode.dark_icon} alt="dark theme icon" class="swap-on w-14 h-5/6 object-contain"/>
         </label>
     </div>
 </div>
+
+<slot />
+
+
+
+<!-- Footer -->
+<footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
+    <nav>
+        <div class="grid grid-flow-col gap-4">
+            <a href="https://github.com/nicojmn">
+                {#if mode === "light"}
+                    <img src={theme.icons.github.dark} alt="github" class="w-10 h-10"/>
+                {:else}
+                    <img src={theme.icons.github.light} alt="github" class="w-10 h-10"/>
+                {/if}
+            </a>
+            <a href="https://gitlab.com/nicojmn">
+                <img src={theme.icons.gitlab} alt="gitlab" class="w-10 h-10"/>
+            </a>
+            <a href="https://linkedin.com/in/nicolas-jeanmenne/">
+                <img src={theme.icons.linkedin} alt="linkedin" class="w-10 h-10"/>
+            </a>
+        </div>
+    </nav> 
+    <aside>
+        <p>
+            Website source code is under <a href="https://opensource.org/license/bsd-3-clause" class="link">free BDS-License</a> 
+            and availaible <a href="https://github.com/nicojmn:nicojmn.github.io" class="link">here</a>.
+        </p>
+    </aside>
+</footer>
 
 <style>
     label {
         height: 85%;
     }
 </style>
-
-<slot />
